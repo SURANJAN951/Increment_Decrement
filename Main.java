74f38da578cae460721cbddfd25991fcd6515629
Suranjan kumar <ksuranjan951@gmail.com>
2023-12-21 21:15:02 +0530

    Increment and Decrement operator in java

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a = 5;
+    System.out.println(a++);
+    int b = 6;
+    System.out.println(++b);
+    int c = 7;
+    System.out.println(c--);
+    int d = 8;
+    System.out.println(--d);
+    int e = 11;
+    e++;
+    System.out.println(e);
+  }
+}
